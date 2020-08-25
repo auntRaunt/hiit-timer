5ff32f5b106d1e5b523f8306828a5465479ff139
oneheadking <50674351+oneheadking@users.noreply.github.com>
2020-08-25 21:37:33 +0800

    Second commit. can pause some timers, but still can not play back

--- public/timerTest.js
+++ public/timerTest.js
@@ -26,6 +26,12 @@ let totalTimeForPass = timeRemainTotal;
 let resetButton = document.getElementById("reset-btn");
 let playPauseButton = document.getElementById("play-start-btn");
 
+//Initiaiztion all timer variable
+let logic_timer = null;
+let remain_timer = null;
+let pass_timer = null;
+let curr_timer = null;
+
 function initTimer(data) {
   currTask.textContent = data[0].name;
   currTime.textContent = data[0].timeFormat;
@@ -33,7 +39,7 @@ function initTimer(data) {
   nextTime.textContent = data[1].timeFormat;
 }
 
-function logicTimer(data, timer) {
+function logicTimer(data) {
   let timing = 0;
   // let logicTimer = timer;
 
@@ -50,7 +56,7 @@ function logicTimer(data, timer) {
     //Make the display second of curr timer count down
     timing += eachTime;
 
-    timer = setTimeout(() => {
+    logic_timer = setTimeout(() => {
       console.log(currDisplayName);
       addInterval();
 
@@ -63,8 +69,8 @@ function logicTimer(data, timer) {
   }
 }
 
-function remainCountDownTimer(timer) {
-  timer = setInterval(() => {
+function remainCountDownTimer() {
+  remain_timer = setInterval(() => {
     timeRemainTotal--;
     document.getElementById("time-remain-p").textContent = new Date(
       timeRemainTotal * 1000
@@ -73,23 +79,9 @@ function remainCountDownTimer(timer) {
       .substr(14, 5);
     console.log(timeRemainTotal);
     if (timeRemainTotal === 0) {
-      clearInterval(timer);
+      clearInterval(remain_timer);
     }
   }, 1000);
-
-  ////Originial Code
-  // timer = setInterval(() => {
-  //   timeRemainTotal--;
-  //   document.getElementById("time-remain-p").textContent = new Date(
-  //     timeRemainTotal * 1000
-  //   )
-  //     .toISOString()
-  //     .substr(14, 5);
-  //   console.log(timeRemainTotal);
-  //   if (timeRemainTotal === 0) {
-  //     clearInterval(timer);
-  //   }
-  // }, 1000);
 }
 
 function addInterval() {
@@ -99,9 +91,9 @@ function addInterval() {
   // console.log(intervalDisplayAdd);
 }
 
-function passAddTimer(timer, second) {
+function passAddTimer(second) {
   let passSecond = second;
-  timer = setInterval(() => {
+  pass_timer = setInterval(() => {
     passSecond++;
     document.getElementById("time-pass-p").textContent = new Date(
       passSecond * 1000
@@ -110,31 +102,33 @@ function passAddTimer(timer, second) {
       .substr(14, 5);
     // console.log(timePassSecond);
     if (passSecond === totalTimeForPass) {
-      clearInterval(timer);
+      clearInterval(pass_timer);
     }
   }, 1000);
+}
 
-  return passSecond;
+function playAllTimer() {
+  initTimer(data);
+  logicTimer(data);
+  remainCountDownTimer();
+  passAddTimer(timePassSecond);
+  currCountDownTimer(curr_timer, eachObjTime, 0);
 }
 
 //Use the response data from server
-function resetAll(data, allTimers) {
-  //Set all the timer to null
-  for (timer of allTimers) {
-    clearInterval(timer);
-    timer = null;
-  }
-  initTimer(data);
+
+function clear_interval(timer) {
+  console.log(timer);
+  clearInterval(timer);
+  timer = null;
+  return timer;
 }
 
-function play(data, allTimers) {
-  let i = 0;
-  let eachObjTime = data[i].time;
-
-  logicTimer(data, allTimers[0]);
-  remainCountDownTimer(allTimers[1], isPaused);
-  passAddTimer(allTimers[2], 0);
-  currCountDownTimer(allTimers[3], eachObjTime, 0);
+function clear_timeout(timer) {
+  console.log(timer);
+  clearTimeout(timer);
+  timer = null;
+  return timer;
 }
 
 //Execute
@@ -146,26 +140,17 @@ async function getData() {
 
   //Initiate variables
   let timePassSecond = 0;
-  let isPaused = false;
   let i = 0;
   let eachObjTime = data[i].time;
 
-  //Initiaiztion all timer variable
-  let logic_timer = -1;
-  let remain_timer = -1;
-  let pass_timer = -1;
-  let curr_timer = -1;
-
-  let all_timers = [logic_timer, remain_timer, pass_timer, curr_timer];
-
   //Think of using recursion inside the curr-time to count down
   function currCountDownTimer(timer, eachObjTime, i) {
-    timer = setInterval(() => {
+    curr_timer = setInterval(() => {
       eachObjTime--;
 
-      //if count down time become 0, clear timer, increase i, count down time becomes data[i].time
+      //if count down time become 0, clear curr_timer, increase i, count down time becomes data[i].time
       if (eachObjTime === 0) {
-        clearInterval(timer);
+        clearInterval(curr_timer);
 
         console.log("eachObjTime = 0");
         console.log(`i = ${i}`);
@@ -179,7 +164,7 @@ async function getData() {
         let newEachObjTime = data[i].time;
 
         //精華所在
-        currCountDownTimer(timer, newEachObjTime, i);
+        currCountDownTimer(curr_timer, newEachObjTime, i);
       }
 
       console.log(`inside the setInterval`);
@@ -197,15 +182,27 @@ async function getData() {
   }
 
   initTimer(data);
-  logicTimer(data, logic_timer);
-  remainCountDownTimer(remain_timer, isPaused);
-  passAddTimer(pass_timer, timePassSecond);
+  logicTimer(data);
+  remainCountDownTimer();
+  passAddTimer(timePassSecond);
   currCountDownTimer(curr_timer, eachObjTime, 0);
 
   playPauseButton.addEventListener("click", () => {
+    // need to toggle click;
     console.log("Pause/Play are clicked?");
 
-    
+    if (remain_timer !== null && logic_timer !== null && pass_timer !== null && curr_timer !== null) {
+      clear_timeout(remain_timer);
+      clear_interval(logic_timer);
+      clear_interval(pass_timer);
+      clear_interval(curr_timer);
+    } else {
+      initTimer(data);
+      logicTimer(data);
+      remainCountDownTimer();
+      passAddTimer(timePassSecond);
+      currCountDownTimer(curr_timer, eachObjTime, 0);
+    }
   });
 
   // resetButton.addEventListener("click", () => {
