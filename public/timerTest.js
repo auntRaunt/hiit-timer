76c3d023d70e811d7be56c5bcbb7a8ed3f05ce43
oneheadking <50674351+oneheadking@users.noreply.github.com>
2020-09-09 18:50:12 +0800

    Solve the problem when page firsst load, all timer will not start until the button is triggered

--- public/timerTest.js
+++ public/timerTest.js
@@ -28,6 +28,9 @@ let totalTimeForPass = timeRemainTotal;
 let resetButton = document.getElementById("reset-btn");
 let playPauseButton = document.getElementById("play-start-btn");
 
+const playIcon = document.getElementById("play-icon");
+const pauseIcon = document.getElementById("pause-icon");
+
 //Initiaiztion all timer variable, should be in global scope, for clear function
 let logic_timer = null;
 let remain_timer = null;
@@ -36,7 +39,10 @@ let curr_timer = null;
 
 let passSecond = 0;
 
-let pause = false;
+let play = false;
+
+let firstPlay = true;
+let counter = 0;
 
 function initTimer(data) {
   currTask.textContent = data[0].name;
@@ -194,24 +200,6 @@ getData(function (data) {
       console.log(`D. currCountDownTimer runs`);
       console.log(`D. newCurrTime = ${newCurrTime}`);
       console.log(`_____________________________`);
-      //if count down time become 0, clear curr_t imer, increase i, count down time becomes data[i].time
-      if (newCurrTime === 0) {
-        clearInterval(curr_timer);
-        if (j === data.length - 1) {
-          document.getElementById("curr-time").textContent = new Date(
-            newCurrTime * 1000
-          )
-            .toISOString()
-            .substr(14, 5);
-          currTask.textContent = "You have finished all the tasks";
-          currTime.textContent = "";
-        }
-        if (j !== data.length) {
-          j++;
-          let newEachObjTime = data[j].time;
-          currCountDownTimer(data, newEachObjTime);
-        }
-      }
 
       //Do not display 0 in text content of curr-time
       if (newCurrTime !== 0) {
@@ -221,20 +209,63 @@ getData(function (data) {
           .toISOString()
           .substr(14, 5);
       }
+
+      //if count down time become 0, clear curr_t imer, increase i, count down time becomes data[i].time
+      if (newCurrTime === 0) {
+        clearInterval(curr_timer);
+        if (j === data.length - 1) {
+          playIcon.style.display = "block";
+          pauseIcon.style.display = "none";
+          currTime.textContent = "";
+          currTask.textContent = "You have finished all the tasks";
+          console.log("Execute this?");
+        }
+
+        if (j !== data.length) {
+          j++;
+          let newEachObjTime = data[j].time;
+          currCountDownTimer(data, newEachObjTime);
+        }
+      }
     }, 1000);
   }
 
-  initTimer(data);
-  logicTimer(data, eachTaskObjTime, false);
-  currCountDownTimer(data, eachCurrObjTime, false);
-  remainCountDownTimer();
-  passAddTimer();
+  // initTimer(data);
+  // logicTimer(data, eachTaskObjTime);
+  // currCountDownTimer(data, eachCurrObjTime);
+  // remainCountDownTimer();
+  // passAddTimer();
 
   playPauseButton.addEventListener("click", () => {
     // need to toggle click;
-    pause = !pause;
+
     if (i !== data.length) {
-      if (pause) {
+      play = !play;
+      if (play) {
+        if (firstPlay) {
+          initTimer(data);
+          logicTimer(data, eachTaskObjTime);
+          currCountDownTimer(data, eachCurrObjTime);
+          remainCountDownTimer();
+          passAddTimer();
+        } else {
+          playIcon.style.display = "none";
+          pauseIcon.style.display = "block";
+          console.log("___________");
+          console.log("RESUME ALL TIMER^_^");
+          passAddTimer(); //pass_timer will start at 0 when resume (solved) ->need to put the pass second in global scope
+          remainCountDownTimer();
+          logicTimer(data, newTaskTime);
+          currCountDownTimer(data, newCurrTime);
+          console.log(`A. timeRemainTotal = ${timeRemainTotal}`);
+          console.log(`B. passSecond = ${passSecond}`);
+          console.log(`C. newTaskTime = ${newTaskTime}`);
+          console.log(`D. newCurrTime = ${newCurrTime}`);
+          console.log(`E. currTime = ${currTime}`);
+        }
+      } else {
+        playIcon.style.display = "block";
+        pauseIcon.style.display = "none";
         console.log("__________");
         console.log("PAUSE ALL TIMER!!!!");
         clear(pass_timer);
@@ -246,22 +277,43 @@ getData(function (data) {
         console.log(`C. newTaskTime = ${newTaskTime}`);
         console.log(`D. newCurrTime = ${newCurrTime}`);
         console.log(`E. currTime = ${currTime}`);
-      } else {
-        console.log("___________");
-        console.log("RESUME ALL TIMER^_^");
-        //is ok
-        passAddTimer(); //pass_timer will start at 0 when resume (solved) ->need to put the pass second in global scope
-        remainCountDownTimer();
-        //not ok
-        logicTimer(data, newTaskTime);
-        currCountDownTimer(data, newCurrTime);
-        console.log(`A. timeRemainTotal = ${timeRemainTotal}`);
-        console.log(`B. passSecond = ${passSecond}`);
-        console.log(`C. newTaskTime = ${newTaskTime}`);
-        console.log(`D. newCurrTime = ${newCurrTime}`);
-        console.log(`E. currTime = ${currTime}`);
+        firstPlay = false;
       }
     }
+
+    ////old code
+    // if (i !== data.length) {
+    //   pause = !pause;
+    //   if (pause) {
+    //     playIcon.style.display = "block";
+    //     pauseIcon.style.display = "none";
+    //     console.log("__________");
+    //     console.log("PAUSE ALL TIMER!!!!");
+    //     clear(pass_timer);
+    //     clear(remain_timer);
+    //     clear(logic_timer); //still can not clear? can not clear the logic timer inside the for loop
+    //     clear(curr_timer);
+    //     console.log(`A. timeRemainTotal = ${timeRemainTotal}`);
+    //     console.log(`B. passSecond = ${passSecond}`);
+    //     console.log(`C. newTaskTime = ${newTaskTime}`);
+    //     console.log(`D. newCurrTime = ${newCurrTime}`);
+    //     console.log(`E. currTime = ${currTime}`);
+    //   } else {
+    //     playIcon.style.display = "none";
+    //     pauseIcon.style.display = "block";
+    //     console.log("___________");
+    //     console.log("RESUME ALL TIMER^_^");
+    //     passAddTimer(); //pass_timer will start at 0 when resume (solved) ->need to put the pass second in global scope
+    //     remainCountDownTimer();
+    //     logicTimer(data, newTaskTime);
+    //     currCountDownTimer(data, newCurrTime);
+    //     console.log(`A. timeRemainTotal = ${timeRemainTotal}`);
+    //     console.log(`B. passSecond = ${passSecond}`);
+    //     console.log(`C. newTaskTime = ${newTaskTime}`);
+    //     console.log(`D. newCurrTime = ${newCurrTime}`);
+    //     console.log(`E. currTime = ${currTime}`);
+    //   }
+    // }
   });
 
   resetButton.addEventListener("click", () => {
